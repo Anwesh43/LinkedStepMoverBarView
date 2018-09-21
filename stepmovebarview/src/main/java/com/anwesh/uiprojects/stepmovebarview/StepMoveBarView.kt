1921fa084aa4536f57ae326cd3622e957c8270ea
Anwesh43 <anweshthecool0@gmail.com>
2018-09-21 10:52:34 +0530

    adding StepMoverBarView class

--- stepmovebarview/src/main/java/com/anwesh/uiprojects/stepmovebarview/StepMoveBarView.kt
+++ stepmovebarview/src/main/java/com/anwesh/uiprojects/stepmovebarview/StepMoveBarView.kt
@@ -13,3 +13,21 @@ import android.graphics.RectF
 import android.graphics.Color
 
 val nodes : Int = 5
+
+class StepMoverBarView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
