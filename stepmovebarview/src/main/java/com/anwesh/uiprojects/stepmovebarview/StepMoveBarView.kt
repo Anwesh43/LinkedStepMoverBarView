6d04eb4eb55ea957fe161d16e04a407a5781b3aa
Anwesh43 <anweshthecool0@gmail.com>
2018-09-21 10:51:05 +0530

    adding libraries and defining nodes

--- stepmovebarview/src/main/java/com/anwesh/uiprojects/stepmovebarview/StepMoveBarView.kt
+++ stepmovebarview/src/main/java/com/anwesh/uiprojects/stepmovebarview/StepMoveBarView.kt
@@ -4,3 +4,12 @@ package com.anwesh.uiprojects.stepmovebarview
  * Created by anweshmishra on 21/09/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
