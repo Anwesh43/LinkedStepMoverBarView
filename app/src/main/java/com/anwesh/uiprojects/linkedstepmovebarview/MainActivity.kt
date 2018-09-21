a579f82c8e5398980980578d5cfc4524b87c2094
Anwesh43 <anweshthecool0@gmail.com>
2018-09-21 11:15:02 +0530

    adding sStepMoverBarView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedstepmovebarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepmovebarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedstepmovebarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.stepmovebarview.StepMoverBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepMoverBarView.create(this)
     }
 }
