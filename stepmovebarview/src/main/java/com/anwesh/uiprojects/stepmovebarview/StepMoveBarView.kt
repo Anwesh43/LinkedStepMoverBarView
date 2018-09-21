2ea002ff4fe70d23ff7a5077ebf48c2b76bf6be7
Anwesh43 <anweshthecool0@gmail.com>
2018-09-21 11:02:07 +0530

    adding drawSMBNode canvas extension method

--- stepmovebarview/src/main/java/com/anwesh/uiprojects/stepmovebarview/StepMoveBarView.kt
+++ stepmovebarview/src/main/java/com/anwesh/uiprojects/stepmovebarview/StepMoveBarView.kt
@@ -14,6 +14,25 @@ import android.graphics.Color
 
 val nodes : Int = 5
 
+fun Canvas.drawSMBNode(i : Int, scale : Float, paint : Paint) {
+    paint.color = Color.parseColor("#4527A0")
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val hSize : Float = gap / 3
+    save()
+    translate((w/2), gap + i * gap)
+    for (j in 0..1) {
+        val sf : Float = 1f - 2 * j
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f * j)) * 2
+        save()
+        translate(-(w/2 - gap) * sf * (1 - sc) - gap + j * gap, 0f)
+        drawRect(RectF(0f, -hSize/2, gap, hSize/2), paint)
+        restore()
+    }
+    restore()
+}
+
 class StepMoverBarView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
